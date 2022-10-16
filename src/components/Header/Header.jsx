b7f1d8d48b09decb0cf51f4de486eabaf905beb2
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-10-16 17:44:43 +0300

    Tour_Places

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.scss";
+
+const Header = () => {
+    return(
+        <div className="header">
+            <h1>Popılar Tour Places</h1>
+        </div>
+    )
+}
+
+export default Header;
